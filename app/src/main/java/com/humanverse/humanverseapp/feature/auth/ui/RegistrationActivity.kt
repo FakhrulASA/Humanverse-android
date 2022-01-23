a7b9a65162195d007f52d4a9fef30912737783a2
fakhrulasa <fakhrulasa@gmail.com>
2022-01-23 13:03:55 +0600

    -theme fix

--- app/src/main/java/com/humanverse/humanverseapp/feature/auth/ui/RegistrationActivity.kt
+++ app/src/main/java/com/humanverse/humanverseapp/feature/auth/ui/RegistrationActivity.kt
@@ -1,13 +1,11 @@
 package com.humanverse.humanverseapp.feature.auth.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
+import android.view.View
 import com.google.firebase.auth.FirebaseAuth
-import com.humanverse.humanverseapp.R
 import com.humanverse.humanverseapp.base.BaseActivity
-import com.humanverse.humanverseapp.databinding.ActivityLoginBinding
 import com.humanverse.humanverseapp.databinding.ActivityRegistrationBinding
 import com.humanverse.humanverseapp.feature.home.ui.ui.HomeActivity
 import com.humanverse.humanverseapp.firebase.AuthImp
@@ -24,6 +22,8 @@ class RegistrationActivity : BaseActivity() {
         authImp = AuthImp()
         auth = FirebaseAuth.getInstance()
         binding.button.setOnClickListener {
+            binding.progressBar.visibility = View.VISIBLE
+            binding.button.text = ""
             verifyRegistration()
         }
 
@@ -33,16 +33,47 @@ class RegistrationActivity : BaseActivity() {
     }
 
     private fun verifyRegistration() {
-        authImp.firebaseRegistration(
-            binding.regTlEmail.text.toString().trim(),
-            binding.regTlConPass.text.toString().trim(),
-            auth,
-            this,
-            {
-                startActivity(Intent(this, HomeActivity::class.java))
-                finish()
-            }, {
-                makeToastLong(it)
-            })
+        if (binding.regTlEmail.text.isNullOrEmpty() || binding.regTlMobile.text.isNullOrEmpty() || binding.regTlConPass.text.isNullOrEmpty() || binding.regTlPass.text.isNullOrEmpty()) {
+            makeToastLong("Please give proper input")
+            binding.progressBar.visibility= View.GONE
+            binding.button.text="REGISTER"
+        } else {
+            if (binding.regTlConPass.text.toString().trim() == binding.regTlPass.text.toString().trim()) {
+                auth.createUserWithEmailAndPassword(
+                    binding.regTlEmail.text.toString().trim(),
+                    binding.regTlConPass.text.toString().trim()
+                )
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            auth.currentUser!!.sendEmailVerification().addOnCompleteListener {
+                                // Sign in success, update UI with the signed-in user's information
+                                if (it.isSuccessful) {
+                                    startActivity(Intent(this, HomeActivity::class.java))
+                                } else {
+                                    makeToastLong(it.exception!!.message.toString())
+                                    binding.progressBar.visibility= View.GONE
+                                    binding.button.text="REGISTER"
+                                }
+                            }
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(
+                                this.toString(),
+                                "createUserWithEmailAndPassword:failure",
+                                task.exception
+                            )
+                            makeToastLong(task.exception!!.message.toString())
+                            binding.progressBar.visibility= View.GONE
+                            binding.button.text="REGISTER"
+                        }
+                    }
+            } else {
+                makeToastLong("Password not matched")
+                binding.progressBar.visibility= View.GONE
+                binding.button.text="REGISTER"
+            }
+        }
+
     }
 }
