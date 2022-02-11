c0b4ef035ebec088423b5c6c7549e10bae007dcf
fakhrulasa <fakhrulasa@gmail.com>
2022-02-12 00:30:47 +0600

    - ongoing item added

---
+++ app/src/main/java/com/humanverse/humanverseapp/model/HistoryServiceModel.kt
@@ -0,0 +1,9 @@
+package com.humanverse.humanverseapp.model
+
+data class HistoryServiceModel(
+    var title: String,
+    var price : Int,
+    var status : String,
+    var datetime: String,
+    var serviceId: String
+)
