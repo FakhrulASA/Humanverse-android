b9c51878391f05a0b26ef32afa4a1f2d418d2a89
fakhrulasa <fakhrulasa@gmail.com>
2022-02-12 00:06:52 +0600

    - order service

--- app/src/main/java/com/humanverse/humanverseapp/feature/service/OrderServiceActivity.kt
+++ app/src/main/java/com/humanverse/humanverseapp/feature/service/OrderServiceActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.humanverse.humanverseapp.R
 import com.humanverse.humanverseapp.databinding.ActivityOrderServiceBinding
 
@@ -16,7 +19,28 @@ class OrderServiceActivity : AppCompatActivity() {
         binding.button10.setOnClickListener{
             startActivity(Intent(this,OrderProgressActivity::class.java))
         }
+        val materialTimePicker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_24H)
+            .build()
 
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select desired date")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build()
+        binding.textView32.setOnClickListener {
+           datePicker.show(supportFragmentManager, "MATERIAL_DATE_PICKER")
+        }
+        binding.textVi2ew32.setOnClickListener {
+            materialTimePicker.show(supportFragmentManager, "MATERIAL_DATE_PICKER")
+        }
+
+        datePicker.addOnPositiveButtonClickListener {
+            binding.textView32.text=datePicker.headerText
+        }
+        materialTimePicker.addOnPositiveButtonClickListener {
+            binding.textVi2ew32.text = materialTimePicker.hour.toString()+":"+materialTimePicker.minute.toString()
+        }
         val b: ArrayAdapter<String> = ArrayAdapter<String>(
             this,
             R.layout.item_spinner,
